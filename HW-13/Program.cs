c080325a6d2e3bd5150f9f09c4b4bed5c4d5aa01
SulaymonZafarov <sulaymonwork@gmail.com>
2021-09-01 16:30:57 +0500

    Add project files.

---
+++ HW-13/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HW_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player player = new Player();
+            Play(player);
+        }
+        public static void Play(IRecPlayable player)
+        {
+            player.playMusic();
+            player.pauseMusic();
+            player.stopMusic();
+            player.startRecord();
+            player.pauseMusic();
+            player.stopMusic();
+        }
+    }
+    interface IPlayable
+    {
+        void playMusic();
+        void pauseMusic();
+        void stopMusic();
+    }
+    interface IRecordable
+    {
+        void startRecord();
+        void pauseRecord();
+        void stopRecord();
+    }
+    interface IRecPlayable : IRecordable, IPlayable
+    {
+    }
+    class Player : IRecPlayable
+    {
+        public void playMusic()
+        {
+            Console.WriteLine("playMusic");
+        }
+        public void pauseMusic()
+        {
+            Console.WriteLine("pauseMusic");
+        }
+        public void stopMusic()
+        {
+            Console.WriteLine("stopMusic");
+        }
+
+        public void startRecord()
+        {
+            Console.WriteLine("startRecord");
+        }
+        public void pauseRecord()
+        {
+            Console.WriteLine("pauseRecord");
+        }
+        public void stopRecord()
+        {
+            Console.WriteLine("stopRecord");
+        }
+    }
+}
